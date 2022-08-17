78544f274ee3c94e960f7a1c214ab1187e5a4c05
Forte <lianghaizhao@icloud.com>
2022-08-17 12:21:43 +0800

    modify user

--- src/main.js
+++ src/main.js
@@ -3,13 +3,12 @@ import router from './router/routes.js'
 import App from './App.vue'
 import 'amfe-flexible'
 
-import { Icon, Button, Toast, Form, Field, } from 'vant'
+import { Icon, Button, Toast, Form, Field, Skeleton } from 'vant'
 
 const app = createApp(App)
 
-app.use(Icon).use(Button).use(Toast).use(Form).use(Field)
+app.use(Icon).use(Button).use(Toast).use(Form).use(Field).use(Skeleton)
 
 app.use(router)
 app.mount('#app')
 
-// Vue.prototype.$md5 = md5;
