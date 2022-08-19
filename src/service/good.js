219f596a42d673d864f61bad2f921d7af9a32595
Forte <lianghaizhao@icloud.com>
2022-08-19 23:41:34 +0800

    add category

---
+++ src/service/good.js
@@ -0,0 +1,13 @@
+import axios from '@/utils/axios'
+
+export function getCategory() {
+  return axios.get('/categories')
+}
+
+export function getDetail(id) {
+  return axios.get(`/goods/detail/${id}`)
+}
+
+export function search(params) {
+  return axios.get('/search', { params })
+}
